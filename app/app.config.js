f7bf6e7d836dcf6366a1c8d823eb6d08f1913cb3
Elya Mikailova <elutic@gmail.com>
2016-11-23 16:04:54 +0300

    start to config routing

--- app/app.config.js
+++ app/app.config.js
@@ -5,7 +5,49 @@
 		.config(configAppRouter)
 		.run(runApp);
 
-	function configAppRouter() {
+	function configAppRouter($routeProvider) {
+		$routeProvider
+			.when("/", {
+				templateUrl: "tasks/task-list.html",
+				controller: "taskList",
+				controllerAs: "$ctrl"
+			})
+			.when("/addTask", {
+				templateUrl: "tasks/add-task.html",
+				controller: "AddTask",
+				controllerAs: "$ctrl"
+			})
+			.when("/users", {
+				templateUrl: "users/user-list.html",
+				controller: "UserList",
+				controllerAs: "$ctrl"
+			})
+			.when("/addUser", {
+				templateUrl: "users/add-user-general.html",
+				controller: "AddUser",
+				controllerAs: "$ctrl"
+			})
+			.when("/addUser/Cost", {
+				templateUrl: "users/add-user-cost.html",
+				controller: "AddUser",
+				controllerAs: "$ctrl"
+			})
+			.when("/addUser/Notes", {
+				templateUrl: "users/add-user-notes.html",
+				controller: "AddUser",
+				controllerAs: "$ctrl"
+			})
+			.when("/editUser", {
+				templateUrl: "users/edit-user.html",
+				controller: "editUser",
+				controllerAs: "$ctrl"
+			})
+			.when("/editUser/:userId", {
+				templateUrl: "user-tasks/user-task-list.html",
+				controller: "userTaskList",
+				controllerAs: "$ctrl"
+			})
+			.otherwise("/");
 	}
 
 	function runApp() {
